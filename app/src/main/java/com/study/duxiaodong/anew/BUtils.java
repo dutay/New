d0ed984ef8b3346f78b65ba0146c030de3396fb8
duxiaodongosc <72161944@bbktel.com>
2022-04-25 15:04:50 +0800

    修改

--- app/src/main/java/com/study/duxiaodong/anew/BUtils.java
+++ app/src/main/java/com/study/duxiaodong/anew/BUtils.java
@@ -3,6 +3,6 @@ package com.study.duxiaodong.anew;
 public class BUtils {
 
     public void test(){
-        System.out.println("test");
+        System.out.println("test==================modify");
     }
 }
