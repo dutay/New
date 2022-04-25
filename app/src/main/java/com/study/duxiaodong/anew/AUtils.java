3882b245744fd69ac4590bba37ea250a6ef40e4c
duxiaodongosc <72161944@bbktel.com>
2022-04-25 14:19:17 +0800

    增加一个工具类AUtils

---
+++ app/src/main/java/com/study/duxiaodong/anew/AUtils.java
@@ -0,0 +1,15 @@
+package com.study.duxiaodong.anew;
+
+public class AUtils {
+
+    public void A(){
+        System.out.println("AAAAAAAAAAAAAAA");
+    }
+
+
+    public void B(){
+        System.out.println("BBBBBBBBBBBBBBB");
+    }
+
+
+}
